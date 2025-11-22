694801a34c3dd8830d9b7a0d126ead6bce019e81
Julia Ovcharenko <samsung2100yuliyu@gmail.com>
2025-11-22 12:01:01 +0200

    first commit: base react structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { createRoot } from "react-dom/client";
+import { App } from ".";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+
+let rootElement = document.querySelector("#root") as HTMLElement
+
+let root = createRoot(rootElement)
+
+root.render(<App></App>)
